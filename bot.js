0f1f99dfef9b85d0dcb4fddac70a0df439db92cb
hassannben <fmomen977@gmail.com>
2026-05-08 01:44:44 +0100

    first telegram bot version

---
+++ bot.js
@@ -0,0 +1,61 @@
+const axios = require("axios");
+
+const TOKEN = "PUT_TOKEN";
+const CHAT_ID = "8486232633";
+
+let lastState = {};
+
+const wilayas = [
+  "الجزائر", "وهران", "قسنطينة", "تيبازة",
+  "البليدة", "سطيف", "عنابة", "تيزي وزو",
+  "بجاية", "باتنة", "الجلفة", "بسكرة"
+];
+
+async function send(msg) {
+  await axios.post(
+    `https://api.telegram.org/bot${TOKEN}/sendMessage`,
+    {
+      chat_id: CHAT_ID,
+      text: msg,
+    }
+  );
+}
+
+async function check() {
+  try {
+    const res = await axios.get("https://adhahi.dz");
+
+    const html = res.data;
+
+    for (let w of wilayas) {
+
+      const pattern = `${w} — حجز غير متوفر`;
+
+      const isAvailable = !html.includes(pattern);
+
+      if (!lastState[w]) lastState[w] = "closed";
+
+      if (isAvailable && lastState[w] === "closed") {
+
+        lastState[w] = "open";
+
+        await send(`🚨 فتح التسجيل في ولاية: ${w}`);
+
+        console.log(w, "OPEN");
+
+      }
+
+      if (!isAvailable) {
+        lastState[w] = "closed";
+      }
+
+    }
+
+  } catch (e) {
+    console.log("error:", e.message);
+  }
+}
+
+setInterval(check, 60000);
+
+console.log("🔥 Smart multi-wilaya bot running...");
